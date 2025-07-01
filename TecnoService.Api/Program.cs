59e74c66d9ed66cac5b0bbc0d845dbf8afc10862
DanielMir <127118475+SilverDani@users.noreply.github.com>
2025-07-01 20:46:04 -0300

    Configuracion de la Conexión

--- TecnoService.Api/Program.cs
+++ TecnoService.Api/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TecnoService.Infraestructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ServiceContext>(options => options.UseSqlServer(connection));
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
