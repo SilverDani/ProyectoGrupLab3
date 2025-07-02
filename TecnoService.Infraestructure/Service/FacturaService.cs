0ee041a9758f731920f8190c98cddb68789e7a95
DanielMir <127118475+SilverDani@users.noreply.github.com>
2025-07-01 21:43:50 -0300

    Infraestructures - Service

---
+++ TecnoService.Infraestructure/Service/FacturaService.cs
@@ -0,0 +1,14 @@
+﻿using TecnoService.Core.Interfaces.Repository;
+using TecnoService.Core.Interfaces.Service;
+using TecnoService.Core.Models;
+
+namespace TecnoService.Infraestructure.Services
+{
+    public class FacturaService : ServiceBase<Factura>, IFacturaService
+    {
+        public FacturaService(IFacturaRepository repFactura) : base(repFactura)
+        {
+
+        }
+    }
+}
---
+++ TecnoService.Infraestructure/Service/PersonaService.cs
@@ -0,0 +1,14 @@
+﻿using TecnoService.Core.Interfaces.Repository;
+using TecnoService.Core.Interfaces.Service;
+using TecnoService.Core.Models;
+
+namespace TecnoService.Infraestructure.Services
+{
+    public class PersonaService : ServiceBase<Persona>, IPersonaService
+    {
+        public PersonaService(IPersonaRepository repPersona) : base(repPersona)
+        {
+
+        }
+    }
+}
