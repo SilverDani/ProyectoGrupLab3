2b4839b4e8eaf6b5041bbd130108021ff3727079
DanielMir <127118475+SilverDani@users.noreply.github.com>
2025-07-01 21:39:05 -0300

    Infraestructure - Repositories

---
+++ TecnoService.Infraestructure/Repositories/ClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using TecnoService.Core.Interfaces.Repository;
+using TecnoService.Core.Models;
+using TecnoService.Infraestructure.Data;
+
+namespace TecnoService.Infraestructure.Repositories
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+        public ClienteRepository(ServiceContext ConCliente) : base(ConCliente)
+        {
+
+        }
+    }
+}
