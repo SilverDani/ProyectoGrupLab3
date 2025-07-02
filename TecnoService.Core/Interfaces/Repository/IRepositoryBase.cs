8a92fd60a8e13ebe95a4216187213e04857abab3
DanielMir <127118475+SilverDani@users.noreply.github.com>
2025-07-01 21:25:38 -0300

    Interface - Repository

---
+++ TecnoService.Core/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace TecnoService.Core.Interfaces.Repository
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int ID);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
