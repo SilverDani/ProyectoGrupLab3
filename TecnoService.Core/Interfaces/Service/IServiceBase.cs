50a6e084d25d21cf803d81346c1b7e6fa4bdbf94
DanielMir <127118475+SilverDani@users.noreply.github.com>
2025-07-01 21:31:20 -0300

    Interfaces - Service

---
+++ TecnoService.Core/Interfaces/Service/IServiceBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TecnoService.Core.Interfaces.Service
+{
+    public interface IServiceBase<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int idID);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int ID);
+    }
+}
